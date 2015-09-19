fe531850cce28fdee469be3852cc828856492630
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-09-19 02:38:19 -0600

    Add Webpack Notifier and BrowserSync Plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var WebpackNotifierPlugin = require('webpack-notifier');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -19,6 +21,14 @@ module.exports = {
       title: 'LDS Music Player',
       template: 'src/index.html',
       favicon: 'extension/icon.png'
+    }),
+    new WebpackNotifierPlugin({
+      alwaysNotify: true
+    }),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['build'] }
     })
   ]
 };
