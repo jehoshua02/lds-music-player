a543f6f2b96fb4f4e591fa5d8f4739f1b414577f
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-10-01 00:31:25 -0600

    Get React Router Working
    Documentation shows 1.0.0-rc1, but npm installed 0.13.x

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require("babel/polyfill");
 var React = require('react');
-var {Router, Route} = require('react-router');
+var {Router, Route, Redirect} = require('react-router');
 var Player = require('components/Player');
+var Song = require('models/Song');
 
 React.render((
   <Router>
-    <Route path="/" component={Player} />
+    <Route path="/:id" component={Player} />
+    <Redirect from="/" to={'/' + Song.random().id.toLowerCase()} />
   </Router>
 ), document.getElementById('app'));
